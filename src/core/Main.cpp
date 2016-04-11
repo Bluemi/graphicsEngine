cbf6bb7d70aba01c3be4e8e57a4e025d811670fe
Bluemi <alok-musik@arcor.de>
2016-04-11 15:24:54 +0200

    added Main-Loop

--- src/core/Main.cpp
+++ src/core/Main.cpp
@@ -5,8 +5,35 @@
 Main::Main()
 {
 	std::cout << " --- GraphicsEngine ---" << std::endl;
+	run();
 }
 
 void Main::run()
 {
+	sf::Clock clock;
+	clock.restart();
+
+	while (running)
+	{
+		if (clock.getElapsedTime().asMilliseconds() >= FRAME_RATE)
+		{
+			clock.restart();
+			tick();
+		}
+	}
+}
+
+void Main::tick()
+{
+	
+}
+
+void Main::render()
+{
+	
+}
+
+void Main::exit()
+{
+	running = false;
 }
--- src/core/Main.hpp
+++ src/core/Main.hpp
@@ -1,12 +1,19 @@
 #ifndef __MAIN_CLASS__
 #define __MAIN_CLASS__
 
+#include <SFML/Graphics.hpp>
+
 class Main
 {
 	public:
 		Main();
 	private:
 		void run();
+		void exit();
+		void tick();
+		void render();
+		bool running = true;
+		int const FRAME_RATE = 20;
 };
 
 #endif
